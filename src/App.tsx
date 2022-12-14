218b859309295cd921decbd312ae3795afd16124
Spookyless <spookyless.info@gmail.com>
2022-12-14 15:45:38 +0100

    Formatting

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,28 @@
+import { Button } from '@mui/material';
+import { ThemeProvider, createTheme } from '@mui/material';
 import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
+function Inside() {
+    return (
+        <div className="App">
+            <Button
+                variant="contained"
+            >
+                Text
+            </Button>
+        </div>
+    )
+}
 
-    </div>
-  );
+const theme = createTheme();
+
+// Providers
+function App() {
+    return (
+        <ThemeProvider theme={theme}>
+            <Inside />
+        </ThemeProvider >
+    );
 }
 
 export default App;
